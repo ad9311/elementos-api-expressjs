3c74e0ff9776947491adecc127c8c9b14c615571
Ángel Díaz <diazangel9311@gmail.com>
2023-10-01 00:23:41 -0500

    Add elements.json
    Create types for element

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,31 @@
 // Element
+export type ElementSeries =
+  | 'nonmetal'
+  | 'alkali_metal'
+  | 'alkaline_earth_metal'
+  | 'transition_metal'
+  | 'post_transition_metal'
+  | 'halogen'
+  | 'noble_gas';
+
+export type ElementState = 'solid' | 'liquid' | 'gas';
+
+export type ElementDiscoveryYear = number | 'Ancient';
+
+export interface Element {
+  symbol: string;
+  name: string;
+  atomicNumber: number;
+  atomicWeight: number;
+  series: ElementSeries;
+  seriesColor: string;
+  description: string;
+  state: ElementState;
+  group: number | null;
+  period: number;
+  density: number | null;
+  meltingPoint: number | null;
+  boilingPoint: number | null;
+  yearDiscovered: ElementDiscoveryYear;
+  infoUrl: string;
+}
